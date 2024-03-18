bd060841dc9b748d190d7f3a79d9ed690dd2b13a
IsAppTask-1- <abdo13pop@gmail.com>
2024-03-18 15:40:21 +0200

    first commit

---
+++ app/src/main/java/com/letspracticeskills/abdullahahmed/calculateagemvpyt/presenter/ICalculateAgePresenter.kt
@@ -0,0 +1,6 @@
+package com.letspracticeskills.abdullahahmed.calculateagemvpyt.presenter
+
+interface ICalculateAgePresenter {
+
+    fun calculateYourAge(name: String, age: String)
+}
---
+++ app/src/main/java/com/letspracticeskills/abdullahahmed/calculateagemvpyt/view/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.letspracticeskills.abdullahahmed.calculateagemvpyt.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.letspracticeskills.abdullahahmed.calculateagemvpyt.R
+import com.letspracticeskills.abdullahahmed.calculateagemvpyt.model.Person
+import com.letspracticeskills.abdullahahmed.calculateagemvpyt.presenter.CalculateAgePresenter
+import com.letspracticeskills.abdullahahmed.calculateagemvpyt.presenter.ICalculateAgePresenter
+
+class MainActivity : AppCompatActivity(), CalculateAgePresenter.View {
+
+
+            // Abdullah Ahmed Abdelnour Younis
+
+
+    lateinit var calculateAgePresenter: ICalculateAgePresenter
+
+    lateinit var textViewInfo: TextView
+    lateinit var editTextAge: EditText
+    lateinit var editTextName: EditText
+    lateinit var buttonCalculateAge: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        calculateAgePresenter = CalculateAgePresenter(this)
+
+        textViewInfo = findViewById(R.id.textView_your_age)
+        editTextName = findViewById(R.id.editText_name)
+        editTextAge = findViewById(R.id.editText_age)
+        buttonCalculateAge = findViewById(R.id.button_calculate_age)
+
+        buttonCalculateAge.setOnClickListener {
+
+            val name = editTextName.text.trim().toString()
+            val age = editTextAge.text.trim().toString()
+
+            calculateAgePresenter.calculateYourAge(name, age)
+        }
+
+    }
+
+    override fun getInfo(person: Person) {
+        textViewInfo.text = "${person.name} your age: ${person.age}"
+    }
+}
